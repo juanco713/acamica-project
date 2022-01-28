2e1135993d506519fe167a71ac6a58abf64b1cfc
Juan Gonzalez <juanco7133@gmail.com>
2022-01-28 15:49:10 -0300

    Models added

---
+++ src/database/models/users.js
@@ -0,0 +1,45 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../index');
+
+const User = sequelize.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    adress: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+module.exports = User;
