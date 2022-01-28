a64c73ed98831631bdae244716925a1015cc51c3
Juan Gonzalez <juanco7133@gmail.com>
2022-01-28 15:37:08 -0300

    Server and connection to db added

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const sequelize = require('./src/database/index.js');
+
+
+async function serverConnection() {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log('Connection has been established');
+        app.use(express.json());
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running in port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+};
+
+
+serverConnection();
